4b1dc48f7e5286fd047a3b62c8b22d70dd4b5b44
Shokhjakhon Komiljonov <komiljonovshohjahon@gmail.com>
2025-01-08 22:41:23 +0500

    update

--- test/nullable_callback_detection/nullable_callback_detection_unsound_test.dart
+++ test/nullable_callback_detection/nullable_callback_detection_unsound_test.dart
@@ -1,4 +1,4 @@
-// @dart=2.9
+// @dart=2.12
 @TestOn('browser')
 library react.test.unsound_null_safety_test;
 
@@ -15,27 +15,40 @@ main() {
   // both in sound and unsound null safety runtime environments.
   //
   // So, this test file simulates that setup.
-  group('nullable callback detection, when compiled with unsound null safety:', () {
+  group('nullable callback detection, when compiled with unsound null safety:',
+      () {
     setUpAll(() {
-      expect(hasUnsoundNullSafety, isTrue, reason: 'these tests should be running under unsound null safety');
+      expect(hasUnsoundNullSafety, isTrue,
+          reason: 'these tests should be running under unsound null safety');
     });
 
     group('isRefArgumentDefinitelyNonNullable:', () {
       group('when refs are declared in null-safe code,', () {
         test('returns false, even for non-nullable refs', () {
-          expect(isRefArgumentDefinitelyNonNullable(null_safe_refs.nonNullableElementRef), isFalse);
+          expect(
+              isRefArgumentDefinitelyNonNullable(
+                  null_safe_refs.nonNullableElementRef),
+              isFalse);
         });
 
         test('returns false for nullable refs', () {
-          expect(isRefArgumentDefinitelyNonNullable(null_safe_refs.nullableElementRef), isFalse);
-          expect(isRefArgumentDefinitelyNonNullable(null_safe_refs.dynamicRef), isFalse);
+          expect(
+              isRefArgumentDefinitelyNonNullable(
+                  null_safe_refs.nullableElementRef),
+              isFalse);
+          expect(isRefArgumentDefinitelyNonNullable(null_safe_refs.dynamicRef),
+              isFalse);
         });
       });
 
       group('when refs are declared in non-null-safe code,', () {
         test('returns false', () {
-          expect(isRefArgumentDefinitelyNonNullable(non_null_safe_refs.elementRef), isFalse);
-          expect(isRefArgumentDefinitelyNonNullable(non_null_safe_refs.dynamicRef), isFalse);
+          expect(
+              isRefArgumentDefinitelyNonNullable(non_null_safe_refs.elementRef),
+              isFalse);
+          expect(
+              isRefArgumentDefinitelyNonNullable(non_null_safe_refs.dynamicRef),
+              isFalse);
         });
       });
     });
